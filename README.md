48bf6ddfa2f60c5067e896900a051d5e46cea9ff
Nick Young <nyoung@atlassian.com>
2019-03-04 12:48:01 +1100

    Fix: default port now 8080, add TODOs

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # frinkahedron
 
-<quote> [/ <length> [<offset]] [| <overlay text>]
+`<quote> [/ <length> [<offset]] [| <overlay text>]`
+
+
+## TODO
+
+- Add support for checking the validity of the messages using https://api.slack.com/docs/verifying-requests-from-slack
+- Deploy to Micros somehow
+- rename the endpoint to something better
+
+then, later:
+- port the gif editing functionality from humorbot
+
+
--- cmd/frinkahedron/frinkahedron.go
+++ cmd/frinkahedron/frinkahedron.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 
-	"github.com/nlopes/slack"
 	"github.com/youngnick/frinkahedron/pkg/slackbot"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
@@ -23,39 +21,8 @@ func main() {
 	kingpinApp.HelpFlag.Short('h')
 	kingpin.MustParse(kingpinApp.Parse(os.Args[1:]))
 
-	http.HandleFunc("/slash", func(w http.ResponseWriter, r *http.Request) {
-		s, err := slack.SlashCommandParse(r)
-		fmt.Printf("%+v", s)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		// if !s.ValidateToken(verificationToken) {
-		// 	fmt.Printf("%v", verificationToken)
-
-		// 	w.WriteHeader(http.StatusUnauthorized)
-		// 	return
-		// }
-
-		switch s.Command {
-		case "/echo":
-			params := &slack.Msg{Text: s.Text}
-			b, err := json.Marshal(params)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.Write(b)
-		default:
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-	})
-
 	http.HandleFunc("/stridestyle", slackbot.StrideStyle)
 
 	fmt.Println("[INFO] Server listening")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":8080", nil)
 }
